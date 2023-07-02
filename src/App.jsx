be73d7965be3a8fc3496341af893e479f2fa0cf2
Vandana Bhavsar <vrab2010@gmail.com>
2023-07-02 22:28:37 +0530

    fetching data with useQuery

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import Home from './Pages/Home';
@@ -6,19 +7,23 @@ import Starred from './Pages/Starred1';
 import MainLayout from './Components/MainLayout';
 import Show from './Pages/Show';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/ContactUs" element={<ContactUs />}></Route>
-          <Route path="/Starred1" element={<Starred />}></Route>
-        </Route>
-        <Route path="/Show/:showId" element={<Show />}></Route>
-        <Route path="*" element={<div>Page Not Found</div>}></Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/ContactUs" element={<ContactUs />}></Route>
+            <Route path="/Starred1" element={<Starred />}></Route>
+          </Route>
+          <Route path="/Show/:showId" element={<Show />}></Route>
+          <Route path="*" element={<div>Page Not Found</div>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
