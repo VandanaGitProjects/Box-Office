7f9915f4307e50bfa5454a3c0697f7c47f13f141
Vandana Bhavsar <vrab2010@gmail.com>
2023-07-19 22:17:08 +0530

    added intial logic to star shows with useReducer. created usePersistedReducer to sync state with localstoragae

--- src/Components/shows/ShowsGrid.jsx
+++ src/Components/shows/ShowsGrid.jsx
@@ -1,6 +1,47 @@
+import { useReducer, useEffect } from 'react';
 import ShowCard from './ShowCard';
 
+const usePersistedReducer = (reducer, intialState, localStorageKey) => {
+  const [state, dispatch] = useReducer(reducer, intialState, initial => {
+    const persisedValue = localStorage.getItem(localStorageKey);
+
+    return persisedValue ? JSON.parse(persisedValue) : initial;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(localStorageKey, JSON.stringify(state));
+  }, [state, localStorageKey]);
+
+  return [state, dispatch];
+};
+//usereducer
+const starredShowReducer = (currentStarred, action) => {
+  switch (action.type) {
+    case 'STAR':
+      return currentStarred.concat(action.showId);
+    case 'UNSTAR':
+      return currentStarred.filter(showId => showId !== action.showId);
+    default:
+      return currentStarred;
+  }
+};
+
 const ShowsGrid = ({ shows }) => {
+  const [starredShows, dispatchStarred] = usePersistedReducer(
+    starredShowReducer,
+    [],
+    'starredShows'
+  );
+
+  const onStarMeClick = showId => {
+    const isStarred = starredShows.includes(showId);
+    if (isStarred) {
+      dispatchStarred({ type: 'UNSTAR', showId });
+    } else {
+      dispatchStarred({ type: 'STAR', showId });
+    }
+  };
+
   return (
     <div>
       {shows.map(data => (
@@ -12,6 +53,7 @@ const ShowsGrid = ({ shows }) => {
             data.show.image ? data.show.image.medium : '/not-found-image.png'
           }
           summary={data.show.summary}
+          onStarMeClick={onStarMeClick}
         />
       ))}
     </div>
